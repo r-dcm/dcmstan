6ba89a09583c884793e841158ce8e6b24afcc14b
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2024-12-19 11:27:39 -0500

    unit tests for checking hierarchy

--- tests/testthat/test-utils-hierarchy.R
+++ tests/testthat/test-utils-hierarchy.R
@@ -19,3 +19,17 @@ test_that("get_att_labels function works - no identifier", {
 
   testthat::expect_equal(labels, exp_labels)
 })
+
+test_that("check_hierarchy", {
+  err <- rlang::catch_cnd(check_hierarchy(2))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "a single string")
+
+  err <- rlang::catch_cnd(check_hierarchy("x <-> y -> z"))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "be a hierarchical structure with a clear starting attribute")
+
+  err <- rlang::catch_cnd(check_hierarchy("x -> y <-> z"))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "be a hierarchical structure with a clear ending attribute")
+})
