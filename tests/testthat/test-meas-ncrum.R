3516aa38309d0b42678a8f56c513c3cb0a3aaf1a
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-01-10 11:27:43 -0500

    adding unit tests

---
+++ tests/testthat/test-meas-ncrum.R
@@ -0,0 +1,29 @@
+test_that("ncrum script works", {
+  ecpe_spec <- dcm_specify(qmatrix = dcmdata::ecpe_qmatrix,
+                           identifier = "item_id",
+                           measurement_model = ncrum())
+  mdm_spec <- dcm_specify(qmatrix = dcmdata::mdm_qmatrix,
+                          identifier = "item",
+                          measurement_model = ncrum())
+  dtmr_spec <- dcm_specify(qmatrix = dcmdata::dtmr_qmatrix,
+                           identifier = "item",
+                           measurement_model = ncrum())
+  expect_snapshot(generate_stan(ecpe_spec))
+  expect_snapshot(generate_stan(mdm_spec))
+  expect_snapshot(generate_stan(dtmr_spec))
+
+  ecpe_spec2 <- dcm_specify(qmatrix = dcmdata::ecpe_qmatrix,
+                            identifier = "item_id",
+                            measurement_model = ncrum(),
+                            structural_model = independent())
+  expect_snapshot(generate_stan(ecpe_spec2))
+
+  # edge case where all items are simple structure
+  dtmr_spec2 <- dcm_specify(qmatrix = dcmdata::dtmr_qmatrix %>%
+                              dplyr::filter(!(item %in% c("10b", "10c", "13",
+                                                          "14", "15a", "17",
+                                                          "18", "22"))),
+                            identifier = "item",
+                            measurement_model = ncrum())
+  expect_snapshot(generate_stan(dtmr_spec2))
+})
