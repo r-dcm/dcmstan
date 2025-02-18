6e86a1328353a7d85079d390cbdc6f5726b403e3
Jake Thompson <wjakethompson@gmail.com>
2025-02-18 15:28:58 -0600

    resolve R CMD CHECK

--- R/zzz-methods-create-profiles.R
+++ R/zzz-methods-create-profiles.R
@@ -8,7 +8,10 @@
 #'   defines attribute relationships (e.g., a
 #'   [structural model][structural-model] or
 #'   [model specification][dcm_specify()]).
-#' @param ... Additional arguments passed to methods.
+#' @param ... Additional arguments passed to methods. See details.
+#'
+#' @details
+#' Additional arguments passed to methods:
 #'
 #' @return A [tibble][tibble::tibble-package] with all possible attribute
 #'   patterns. Each row is a profile, and each column indicates whether
@@ -17,7 +20,9 @@
 #'
 #' @examples
 #' create_profiles(3L)
+#'
 #' create_profiles(5)
+#'
 #' create_profiles(unconstrained(), attributes = 2)
 create_profiles <- S7::new_generic("create_profiles", "x")
 
@@ -39,7 +44,8 @@ S7::method(create_profiles, S7::class_numeric) <- function(x) {
     tibble::as_tibble()
 }
 
-#' @param keep_names When `x` is a [model specification][dcm_specify()], should
+#' @details
+#' `keep_names`: When `x` is a [model specification][dcm_specify()], should
 #'   the real attribute names be used (`TRUE`; the default), or replaced with
 #'   generic names (`FALSE`; e.g., `"att1"`, `"att2"`, `"att3"`).
 #' @name create_profiles
@@ -57,7 +63,8 @@ S7::method(create_profiles, dcm_specification) <-
     profs
   }
 
-#' @param attributes When `x` is a [structural model][structural-model], the
+#' @details
+#' `attributes`: When `x` is a [structural model][structural-model], the
 #'   number of attributes that should be used to generate the profiles.
 #' @name create_profiles
 S7::method(create_profiles, structural) <- function(x, attributes) {
