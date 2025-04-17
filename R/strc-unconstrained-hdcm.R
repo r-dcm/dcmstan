029d8f0074d35d6975874cc3a30b6857a5bd506a
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-04-17 11:40:46 -0400

    remove lint

--- R/strc-unconstrained-hdcm.R
+++ R/strc-unconstrained-hdcm.R
@@ -39,9 +39,9 @@ strc_unconstrained <- function(qmatrix, priors) {
     ) |>
     dplyr::pull("prior_def")
 
-  return(list(parameters = parameters_block,
-              transformed_parameters = transformed_parameters_block,
-              priors = strc_priors))
+  list(parameters = parameters_block,
+       transformed_parameters = transformed_parameters_block,
+       priors = strc_priors)
 }
 
 #' @rdname lcdm-crum
