355536d708d47d0bcd2f385624c97a1cfbd0fbc4
Jake Thompson <wjakethompson@gmail.com>
2025-04-23 11:19:12 -0600

    resovle hierarchy edge cases

--- R/utils-hierarchy.R
+++ R/utils-hierarchy.R
@@ -7,12 +7,14 @@
 check_hierarchy <- function(x, allow_null = TRUE,
                             arg = rlang::caller_arg(x),
                             call = rlang::caller_env()) {
-  if (is.null(x) && allow_null) return(x)
+  if (is.null(x) && allow_null) return(invisible(NULL))
 
   check_string(x)
 
   g <- glue::glue(" graph { <x> } ", .open = "<", .close = ">")
   g <- dagitty::dagitty(g)
+
+  if (nrow(dagitty::edges(g)) == 0) return(invisible(NULL))
 
   hierarchy <- glue::glue(" dag { <x> } ", .open = "<", .close = ">")
   hierarchy <- ggdag::tidy_dagitty(hierarchy)
@@ -27,6 +29,50 @@ check_hierarchy <- function(x, allow_null = TRUE,
     rdcmchecks::abort_bad_argument(arg = arg,
                                    must = "not be cyclical",
                                    call = call)
+  }
+
+  invisible(NULL)
+}
+
+check_hierarchy_names <- function(x, attribute_names,
+                                  arg = rlang::caller_arg(x),
+                                  call = rlang::caller_env()) {
+  check_string(x)
+  check_character(attribute_names)
+
+  g <- dagitty::dagitty(glue::glue("dag {{ {x} }}"))
+  hier_names <- if (nrow(dagitty::edges(g)) == 0) {
+    x
+  } else {
+    g |>
+      ggdag::as_tidy_dagitty() |>
+      tibble::as_tibble() |>
+      dplyr::pull("name")
+  }
+
+  extr_hier <- setdiff(hier_names, attribute_names)
+  miss_hier <- setdiff(attribute_names, hier_names)
+
+  if (length(extr_hier)) {
+    rdcmchecks::abort_bad_argument(
+      arg = arg,
+      must = "only include attributes in a hierarchy present in the Q-matrix",
+      footer = cli::format_message(
+        c(i = "Extra attributes: {.val {extr_hier}}")
+      ),
+      call = call
+    )
+  }
+
+  if (length(miss_hier)) {
+    rdcmchecks::abort_bad_argument(
+      arg = arg,
+      must = "include all attributes in a hierarchy present in the Q-matrix",
+      footer = cli::format_message(
+        c(i = "Missing attributes: {.val {miss_hier}}")
+      ),
+      call = call
+    )
   }
 
   invisible(NULL)
--- tests/testthat/test-utils-hierarchy.R
+++ tests/testthat/test-utils-hierarchy.R
@@ -1,27 +1,9 @@
-test_that("get_att_labels function works - identifier", {
-  exp_labels <- tibble::tibble(att = c("att1", "att2", "att3"),
-                               att_label = c("morphosyntactic", "cohesive",
-                                             "lexical"))
-
-  labels <- get_att_labels(dcmdata::ecpe_qmatrix,
-                           identifier = "item_id")
-
-  testthat::expect_equal(labels, exp_labels)
-})
-
-test_that("get_att_labels function works - no identifier", {
-  exp_labels <- tibble::tibble(att = c("att1", "att2", "att3"),
-                               att_label = c("morphosyntactic", "cohesive",
-                                             "lexical"))
-
-  labels <- get_att_labels(dcmdata::ecpe_qmatrix %>%
-                             dplyr::select(-"item_id"))
-
-  testthat::expect_equal(labels, exp_labels)
-})
-
 test_that("check_hierarchy", {
   err <- rlang::catch_cnd(check_hierarchy(2))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "a single string")
+
+  err <- rlang::catch_cnd(check_hierarchy(c("x -> y", "y -> z")))
   expect_s3_class(err, "rlang_error")
   expect_match(err$message, "a single string")
 
@@ -32,4 +14,79 @@ test_that("check_hierarchy", {
   err <- rlang::catch_cnd(check_hierarchy("a -> b -> c -> d -> b  d -> e"))
   expect_s3_class(err, "rlang_error")
   expect_match(err$message, "not be cyclical")
+
+  expect_null(check_hierarchy("x -> y -> z"))
+})
+
+test_that("check hierarchy names", {
+  err <- rlang::catch_cnd(check_hierarchy_names(3, paste0("att", 1:3)))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "a single string")
+
+  err <- rlang::catch_cnd(check_hierarchy_names("1 -> 2 -> 3", 1:3))
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "a character vector")
+
+  err <- rlang::catch_cnd(
+    check_hierarchy_names("red -> yellow -> blue",
+                          attribute_names = c("red", "yellow"))
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "only include attributes")
+  expect_match(err$footer, "blue")
+
+  err <- rlang::catch_cnd(
+    check_hierarchy_names("red -> yellow -> blue",
+                          attribute_names = c("red", "yellow", "blue", "green"))
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "include all attributes")
+  expect_match(err$footer, "green")
+
+  expect_null(
+    check_hierarchy_names("red -> yellow -> blue",
+                          attribute_names = c("blue", "red", "yellow"))
+  )
+})
+
+test_that("check hierarchy names in specification", {
+  err <- rlang::catch_cnd(
+    dcm_specify(
+      qmatrix = dcmdata::ecpe_qmatrix, identifier = "item_id",
+      structural_model = hdcm("lexcial -> cohesive -> morphosyntactic")
+    )
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "only include attributes")
+  expect_match(err$footer, "lexcial")
+
+  err <- rlang::catch_cnd(
+    dcm_specify(
+      qmatrix = dcmdata::mdm_qmatrix, identifier = "item",
+      structural_model = hdcm("division")
+    )
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "only include attributes")
+  expect_match(err$footer, "division")
+
+  err <- rlang::catch_cnd(
+    dcm_specify(
+      qmatrix = dcmdata::dtmr_qmatrix, identifier = "item",
+      structural_model = hdcm(
+        "referent_units -> partitioning_iterating <- appropriateness"
+      )
+    )
+  )
+  expect_s3_class(err, "rlang_error")
+  expect_match(err$message, "include all attributes")
+  expect_match(err$footer, "multiplicative_comparison")
+
+  expect_true(S7::S7_inherits(
+    dcm_specify(
+      qmatrix = dcmdata::ecpe_qmatrix, identifier = "item_id",
+      structural_model = hdcm("lexical -> cohesive -> morphosyntactic")
+    ),
+    dcm_specification
+  ))
 })
