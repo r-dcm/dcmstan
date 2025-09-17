ac651292a12c152184f8530ea1b643deff111de3
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-09-17 13:45:18 -0400

    test that hdcm doesn't change dina or dino

--- tests/testthat/test-meas-dina-dino.R
+++ tests/testthat/test-meas-dina-dino.R
@@ -33,6 +33,16 @@ test_that("dina script works", {
     structural_model = loglinear()
   )
   expect_snapshot(stan_code(dtmr_dina_logl))
+
+  ecpe_dina_hdcm <- dcm_specify(
+    qmatrix = dcmdata::ecpe_qmatrix,
+    identifier = "item_id",
+    measurement_model = dina(),
+    structural_model = hdcm(
+      hierarchy = "lexical -> cohesive -> morphosyntactic"
+    )
+  )
+  expect_equal(stan_code(ecpe_dina_hdcm), stan_code(ecpe_dina_unst))
 })
 
 test_that("dino script works", {
@@ -70,21 +80,7 @@ test_that("dino script works", {
     structural_model = loglinear()
   )
   expect_snapshot(stan_code(dtmr_dino_logl))
-})
 
-test_that("dina with hierarchy works", {
-  ecpe_dina_hdcm <- dcm_specify(
-    qmatrix = dcmdata::ecpe_qmatrix,
-    identifier = "item_id",
-    measurement_model = dina(),
-    structural_model = hdcm(
-      hierarchy = "lexical -> cohesive -> morphosyntactic"
-    )
-  )
-  expect_snapshot(stan_code(ecpe_dina_hdcm))
-})
-
-test_that("dino with hierarchy works", {
   ecpe_dino_hdcm <- dcm_specify(
     qmatrix = dcmdata::ecpe_qmatrix,
     identifier = "item_id",
@@ -93,5 +89,5 @@ test_that("dino with hierarchy works", {
       hierarchy = "lexical -> cohesive -> morphosyntactic"
     )
   )
-  expect_snapshot(stan_code(ecpe_dino_hdcm))
+  expect_equal(stan_code(ecpe_dino_hdcm), stan_code(ecpe_dino_unst))
 })
