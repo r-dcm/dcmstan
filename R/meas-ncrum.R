94c9f66ffcb02ca1e3c9355e8140557254c0dd86
Jake Thompson <wjakethompson@gmail.com>
2025-11-17 15:25:58 -0600

    resolve lints

--- R/meas-ncrum.R
+++ R/meas-ncrum.R
@@ -8,7 +8,8 @@
 #' @param priors Priors for the model, specified through a combination of
 #'   [default_dcm_priors()] and [prior()].
 #' @param att_names Vector of attribute names, as in the
-#'   `qmatrix_meta$attribute_names` of a [\acronym{DCM} specification][dcm_specify()].
+#'   `qmatrix_meta$attribute_names` of a [\acronym{DCM}
+#'   specification][dcm_specify()].
 #' @param hierarchy Optional. If present, the quoted attribute hierarchy. See
 #'   \code{vignette("dagitty4semusers", package = "dagitty")} for a tutorial on
 #'   how to draw the attribute hierarchy.
